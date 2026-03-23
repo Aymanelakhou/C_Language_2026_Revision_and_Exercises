ff3dd091f58806800ad9de7ad313ab95f33df22a
Aymanelakhou <aymane13lahoul@gmail.com>
2026-03-23 21:52:21 +0100

    first commit

---
+++ C_Language_2026_Revision_and_Exercises.c
@@ -0,0 +1,66 @@
+#include <stdio.h> 
+
+int main () {
+    /*التمرين 1: المتغيرات وأنواع البيانات*/
+   /* int Age;
+    Age = 20;
+    float Height;
+    Height = 1.75;
+    char Grade;
+    Grade = 'A';
+
+    printf("Age:%d\nHeight:%.2f\nGrade:%c" ,Age,Height,Grade);
+    return 0;*/
+
+
+    /*التمرين 2: الثوابت*/
+  /*  const int PI = 20;
+    printf("PI: %d" , PI);
+    return 0;*/
+
+    /*التمرين 3: الإدخال / الإخراج*/
+
+  /*  char name[20] = "ayman";
+    int age = 23;
+    printf("Hello %s you are %d years old." ,name,age);
+    return 0;*/
+
+    /*التمرين 4: العمليات الشرطية*/
+
+    /*int num;
+    printf("entr num;");
+    scanf("%d" ,&num);
+    if (num >= 0)
+    {
+    printf("Positive");
+    }
+
+    else if (num == 0)
+        {
+            printf("Zero");
+      }
+
+    else {
+        printf("Negative");
+    }*/
+   /*التمرين 5: العمليات المنطقية*/
+
+ /**int age ,resull,a,b;
+    printf("Entr You age:");
+    scanf("%d" , &age);
+    if (age >= 13 && age <= 19)
+    {
+        printf("Teenager\n");
+    }
+    else if (age >= 20)
+    {
+        printf("Adult\n");
+    }
+    else {
+        printf("Child\n");
+    }
+
+    return 0;*/
+    
+    }
+    
