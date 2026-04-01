069de0ec4eb52e58ca45db1460d008110286474b
Aymanelakhou <aymane13lahoul@gmail.com>
2026-04-01 19:10:16 +0100

    first commit

---
+++ C_Emax2-3.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+int main () {
+    /*تمرين 1 – Variables*/
+    /*int = 5;
+    printf("%d"  , b);*/
+    /*تمرين 2 – Data Types*/
+    /*int i = 2;
+    float j = 2.52;
+    printf("i=%d\nj=%.2f\n" , i,j);*/
+    /*تمرين 3 – Constants*/
+    /*const int a = 15;
+    printf("%d\n" , a);*/
+    /*تمرين 4 – Input / Output*/
+    /*int num;
+    printf("Entr you number");
+    scanf("%d" , &num);
+    printf("%d\n" , num);*/
+    /*تمرين 5 – Relational Operators*/
+    /*int a,b ,relational;
+    a = 5;
+    b = 2;
+    relational = (a == b) && (b > a);
+    printf("(a == b) && (b > a)%d\n" , relational);
+    relational = (a == b) && (a == b);
+    printf("(a == b) && (a == b)%d\n" , relational);
+    relational = (a > b); 
+    printf("(a > b) || (a > b)%d\n" , relational);*/
+    /*تمرين 6 – Logical Operators*/
+    /*int a = 5 ,b = 8;
+    int redlu;
+    redlu = ( b < a) && (a > b);
+    printf("(b<a)&&(a>b) = %d\n", redlu);
+    redlu = ( b == a) || (a > b);
+    printf("(b==a)||(a>b) = %d\n", redlu);
+    redlu !=(b > a);
+    printf("!(b>a) = %d\n", redlu);
+    /*تمرين 7 – if / if-else*/
+    /*int number;
+    printf("enter you number:");
+    scanf("%d" , &number);
+    if(number > 10) {
+        printf("big");
+    }
+    else {
+        printf("small");
+    }*/
+   /*تمرين 8 – Loops (for & while)*/
+   /*For*/
+    /*for(int i = 1; i <= 10; i++) {
+        printf("%d\n" , i);
+    }*/
+   /*while*/
+   /*int u = 0;
+   while ( u <= 5) {    
+        printf("%d\n" , u);
+        u++;
+        
+   }*/
+    return 0;
+}
